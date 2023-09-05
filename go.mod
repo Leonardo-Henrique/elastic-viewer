31f34de93f99ae2446cd2a87de60ecebf629638f
Leonardo Henrique <leonardoh481@gmail.com>
2023-09-04 21:40:01 -0300

    First commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Leonardo-Henrique/elastic-viewer
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import "log"
+
+func main() {
+
+	log.Println("Elastic Viewer Just Started without any errors")
+
+}
